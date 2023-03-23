d6d58f28481871231d570d94b90d52dab5dbf6f9
Eguo Wang <wangeguo@gmail.com>
2023-03-23 14:30:36 +0800

    Launch without opening terminal

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![windows_subsystem = "windows"]
+
 use desktop::app::App;
 use iced::{window, Application, Settings};
 
