f9d12372b332c8fdd0205b67fa877287c5fd9987
Eguo Wang <wangeguo@gmail.com>
2023-12-28 04:07:10 +0800

    Load data from the network in inspect view, Close #6

--- src/views/detail/inspect.rs
+++ src/views/detail/inspect.rs
@@ -14,74 +14,65 @@
 
 use std::collections::HashMap;
 use std::sync::Arc;
+use std::time::Duration;
 
 use amp_client::playbooks::Playbook;
 use iced::widget::Rule;
 use iced::{Command, Length, Subscription};
 use iced_aw::TabLabel;
+use tracing::{error, trace};
 
 use crate::context::Context;
 use crate::widgets::tabs::Tab;
-use crate::widgets::{Column, Element, Row, Scrollable, Text};
+use crate::widgets::{empty::empty, Column, Element, Row, Scrollable, Text};
 
 #[derive(Clone, Debug)]
-pub enum Message {}
+pub enum Message {
+    Refresh,
+}
 
 pub struct Information {
+    ctx: Arc<Context>,
     data: HashMap<String, HashMap<String, String>>,
+    playbook: Playbook,
 }
 
 impl Information {
-    pub fn new(_ctx: Arc<Context>, _playbook: Playbook) -> Self {
+    pub fn new(ctx: Arc<Context>, playbook: Playbook) -> Self {
         Self {
-            data: HashMap::from([
-                (
-                    "environments".into(),
-                    HashMap::from([
-                        ("K3S_TOKEN".into(), "RdqNLMXRiRsHJhmxKurR".into()),
-                        ("K3S_KUBECONFIG_OUTPUT".into(), "/output/kubeconfig.yaml".into()),
-                        (
-                            "PATH".into(),
-                            "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/bin/aux".into(),
-                        ),
-                        ("CRI_CONFIG_FILE".into(), "/var/lib/rancher/k3s/agent/etc/crictl.yaml".into()),
-                    ]),
-                ),
-                (
-                    "mounts".into(),
-                    HashMap::from([
-                        (
-                            "/VAR/LIB/CNI".into(),
-                            "/var/lib/docker/volumes/00f49631b07ccd74de44d3047d5f889395ac871e05b622890b6dd788d34a59f4/_data".into(),
-                        ),
-                        (
-                            "/VAR/LIB/KUBELET".into(),
-                            "/var/lib/docker/volumes/bc1b16d39a0e204841695de857122412cfdefd0f672af185b1fa43e635397848/_data".into(),
-                        ),
-                        (
-                            "/VAR/LIB/RANCHER/K3S".into(),
-                            "/var/lib/docker/volumes/a78bcb9f7654701e0cfaef4447ef61ced4864e5b93dee7102ec639afb5cf2e1d/_data".into(),
-                        ),
-                        (
-                            "/VAR/LOG".into(),
-                            "/var/lib/docker/volumes/f64c2f2cf81cfde89879f2a17924b31bd2f2e6a6a738f7df949bf6bd57102d25/_data".into(),
-                        ),
-                    ]),
-                ),
-                ("port".into(), HashMap::from([("6443/tcp".into(), "0.0.0.0:42397".into())])),
-            ])
+            ctx,
+            data: Default::default(),
+            playbook,
         }
     }
 
-    pub fn update(&mut self, _message: Message) -> Command<Message> {
+    pub fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::Refresh => {
+                match self.ctx.client.actors().info(&self.playbook.id, "amp-example-go") {
+                    Ok(data) => {
+                        trace!("Fetched actor's info: {:#?}", data);
+                        self.data = serde_json::from_value(data).unwrap();
+                    }
+                    Err(e) => {
+                        error!("Failed to fetch actor's info, error: {}", e);
+                    }
+                };
+            }
+        }
+
         Command::none()
     }
 
     pub fn subscription(&self) -> Subscription<Message> {
-        Subscription::none()
+        iced::time::every(Duration::from_secs(5)).map(|_| Message::Refresh)
     }
 
     pub fn view(&self) -> Element<Message> {
+        if self.data.is_empty() {
+            return empty("No information available").into();
+        }
+
         let mut children = vec![];
 
         for (group, fields) in &self.data {
