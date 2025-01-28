996afe84020063e3d22c9fbc87443451e049528d
Eguo Wang <wangeguo@gmail.com>
2025-01-28 11:02:41 +0800

    fix(lint): replace rand deprecated function with new function

--- src/utils/strings.rs
+++ src/utils/strings.rs
@@ -12,16 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use rand::{thread_rng, Rng};
+use rand::{rng, Rng};
 
 pub fn generate_random_word(length: usize) -> String {
-    let mut rng = thread_rng();
+    let mut rng = rng();
     const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
 
     // 生成随机单词
     let word: String = (0..length)
         .map(|_| {
-            let idx = rng.gen_range(0..CHARSET.len());
+            let idx = rng.random_range(0..CHARSET.len());
             CHARSET[idx] as char
         })
         .collect();
@@ -30,11 +30,11 @@ pub fn generate_random_word(length: usize) -> String {
 }
 
 pub fn get_random_words(count: usize) -> Vec<String> {
-    let mut rng = thread_rng();
+    let mut rng = rng();
     let mut random_words: Vec<String> = Vec::new();
 
     for _ in 0..count {
-        let word_length = rng.gen_range(3..11);
+        let word_length = rng.random_range(3..11);
         random_words.push(generate_random_word(word_length));
     }
 
@@ -42,8 +42,8 @@ pub fn get_random_words(count: usize) -> Vec<String> {
 }
 
 pub fn generate_random_words_string(range: std::ops::Range<usize>) -> String {
-    let mut rng = thread_rng();
-    let count = rng.gen_range(range);
+    let mut rng = rng();
+    let count = rng.random_range(range);
 
     get_random_words(count).join(" ")
 }
